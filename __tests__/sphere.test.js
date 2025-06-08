641f7be47b80cb62d7f0a16efbef47a14f1c2abb
vkarkhan <varad.karkhanis@gmail.com>
2025-06-08 12:49:39 +0530

    Add spinning sphere animation

---
+++ __tests__/sphere.test.js
@@ -0,0 +1,48 @@
+const { JSDOM } = require('jsdom');
+const { initSphereAnimation } = require('../script.js');
+
+describe('spinning sphere', () => {
+  let originalObserver;
+
+  beforeEach(() => {
+    originalObserver = global.IntersectionObserver;
+  });
+
+  afterEach(() => {
+    global.IntersectionObserver = originalObserver;
+    delete global.window;
+    delete global.document;
+  });
+
+  test('adds spin class on intersection', () => {
+    const dom = new JSDOM(`<!DOCTYPE html><img id="spinningSphere">`);
+    const { window } = dom;
+    global.window = window;
+    global.document = window.document;
+
+    let capturedCb;
+    class MockObserver {
+      constructor(cb) { capturedCb = cb; }
+      observe() {}
+      unobserve() {}
+    }
+    global.IntersectionObserver = MockObserver;
+
+    const { sphere } = initSphereAnimation();
+    capturedCb([{ isIntersecting: true, target: sphere }]);
+    expect(sphere.classList.contains('spin')).toBe(true);
+  });
+
+  test('toggles spin class on click', () => {
+    const dom = new JSDOM(`<!DOCTYPE html><img id="spinningSphere" class="spin">`);
+    const { window } = dom;
+    global.window = window;
+    global.document = window.document;
+    class MockObserver { constructor(){} observe(){} unobserve(){} }
+    global.IntersectionObserver = MockObserver;
+
+    const { sphere } = initSphereAnimation();
+    sphere.dispatchEvent(new window.Event('click'));
+    expect(sphere.classList.contains('spin')).toBe(false);
+  });
+});
